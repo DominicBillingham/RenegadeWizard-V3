567b8eea5d0dd958d994414aabb7d1bfabcb9b45
Dominic Billingham <dominicbillingham3232@gmail.com>
2026-01-21 22:11:45 +0000

    Tidied up the damage effect. Also death should be an effect at some point!

--- RenegadeWizardWasm/Core/Interactions/Effects/DamageEffect.cs
+++ RenegadeWizardWasm/Core/Interactions/Effects/DamageEffect.cs
@@ -5,10 +5,16 @@ public class DamageEffect : InteractionEffect
     public int Damage { get; set; }
     protected override void Core()
     {
+        if (Target.Hitpoints < 1)
+        {
+            Result = $"but {Target.Name} has already been destroyed!";
+            return;
+        }
+        
         Result += $" {Target.Name} takes {Damage} damage.";
         Target.Hitpoints -= Damage;
 
-        if (Target.Hitpoints < 0)
+        if (Target.Hitpoints <= 0)
         {
             Result += $"{Target.Name} <destroyed>";
         }
