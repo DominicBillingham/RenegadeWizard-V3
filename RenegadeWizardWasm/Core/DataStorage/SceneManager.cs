4a793d854c86e70f1b1844858b57af84bdd881dd
Dominic Billingham <dominicbillingham3232@gmail.com>
2026-01-24 17:02:02 +0000

    Added levels that can be loaded.

--- RenegadeWizardWasm/Core/DataStorage/SceneManager.cs
+++ RenegadeWizardWasm/Core/DataStorage/SceneManager.cs
@@ -6,8 +6,8 @@ namespace RenegadeWizardWasm.Core.DataStorage;
 
 public class SceneManager
 {
-    private List<Entity> _entities = new();
-    public IReadOnlyList<Entity> Entities => _entities;
+    private List<Entity> _entities { get; set; } = new();
+    public IReadOnlyCollection<Entity> Entities => _entities;
     public Entity Player => Entities.First(x => x.Controller == Controller.Player);
     public List<Entity> Npcs => Entities.Where(x => x.Controller == Controller.Npc).ToList();
     public List<Entity> Objects => Entities.Where(x => x.Controller == Controller.Object).ToList();
@@ -28,6 +28,33 @@ public class SceneManager
         
         _entities.Add(new Door());
         _entities.Add(new Chair());
+    }
+
+    public string Level1()
+    {
+        var tempPlayer = Player;
+        _entities.Clear();
+        _entities.Add(tempPlayer);
+        _entities.Add(new Door()); 
+        return "Level 1 loaded";
+    }
+    
+    public string Level2()
+    {
+        var tempPlayer = Player;
+        _entities.Clear();
+        _entities.Add(tempPlayer);
+        _entities.Add(new Goose()); 
+        return "Level 2 loaded";
+    }
+    
+    public string Level3()
+    {
+        var tempPlayer = Player;
+        _entities.Clear();
+        _entities.Add(tempPlayer);
+        _entities.Add(new Goblin()); 
+        return "Level 3 loaded";
     }
     
     public void RemoveDestroyedEntities()
